aa0b42cef970f3aafa4d06f2a10415c71ff3d4ac
Sergey Rogovtsev <srogovtsev@acumatica.com>
2024-01-23 14:31:13 -0500

    Re serilog-tracing/serilog-tracing#34: bare otel implementation

---
+++ example/ActivityTracing/Library/Worker.cs
@@ -0,0 +1,21 @@
+﻿namespace ActivityTracing.Library;
+
+internal class Worker(ILogger<Worker> logger)
+{
+    //usually this would be separate, but let's pretend
+    public const string ActivitySourceName = "MyLibrary";
+
+    private static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+
+    private readonly Guid _id = Guid.NewGuid();
+
+    public void Run()
+    {
+        // ReSharper disable once ExplicitCallerInfoArgument
+        using var activity = ActivitySource.StartActivity(name: "Worker Run", kind: ActivityKind.Internal, tags: new Dictionary<string, object?>
+        {
+            {"WorkerId", _id}
+        });
+        logger.LogInformation("Worker {WorkerId} was run", _id);
+    }
+}
---
+++ example/ActivityTracing/Program.cs
@@ -0,0 +1,32 @@
+﻿using ActivityTracing.Library;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using Serilog;
+
+const string programSourceName = "Program";
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(programSourceName)
+    .AddSource(Worker.ActivitySourceName)
+    .AddOtlpExporter()
+    .Build();
+
+using var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+using var loggerFactory = LoggerFactory
+    .Create(builder => builder.AddSerilog(logger));
+
+//end of configuration, let's go
+
+using var source = new ActivitySource(programSourceName);
+
+using var root = source.StartActivity();
+
+logger.Information("Started");
+
+new Worker(loggerFactory.CreateLogger<Worker>()).Run();
+new Worker(loggerFactory.CreateLogger<Worker>()).Run();
+
+logger.Information("Finished");
